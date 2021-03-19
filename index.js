d3d7c3426c9ae3aa028a000da1e5de23652b260c
公子羽 <63553100@qq.com>
2021-03-19 12:51:49 +0800

    .

--- index.js
+++ index.js
@@ -7005,7 +7005,7 @@
                 this.cellh1.totText.text = "累计灌注 [TF]: " + format(cells[choseY][choseX].totFlush, 2) + " 点数";
                 this.cellh1.mfText.text = "手动灌注 [MF]: " + format(cells[choseY][choseX].flushed, 2) + " 点数";
                 this.cellh1.nlText.text = "下一级将于: " + format(cells[choseY][choseX].nextLat, 0) + " MF";
-                this.cellh1.lmText.text = "等级倍数 " + format(cells[choseY][choseX].lMult, 2);
+                this.cellh1.lmText.text = "等级倍数 [LM] " + format(cells[choseY][choseX].lMult, 2);
                 this.cellh2.Formulat.text = "倍数 = 1 + [log4(1 + TF/CD)^PE]/10*LM";
                 this.cellh2.cdText.text = "网格削弱 [CD]: " + format(cells[choseY][choseX].fDecr, 0);
                 if (choseY == 0 && choseX == 0) {
@@ -7025,7 +7025,7 @@
                     this.cellh2.crossUp.visible = false;
                     this.cellh2.crossLeft.visible = false;
                 }
-                this.cellh2.preExp.text = "网格重置倍数[PE]: ^" + format_short(cells[choseY][choseX].presExp);
+                this.cellh2.preExp.text = "网格重置倍数 [PE]: ^" + format_short(cells[choseY][choseX].presExp);
                 if (cells[choseY][choseX].flushed.lt(cells[choseY][choseX].flushToP)) {
                     this.cellh2.preSend.visible = false;
                     this.cellh2.preExc.visible = false;
